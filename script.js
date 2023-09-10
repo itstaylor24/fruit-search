624382fcea60830d69df75defd22ee03adc8c8db
Taylor Harris <itstaylor24@yahoo.com>
2023-09-10 13:42:04 -0400

    added html css and js

---
+++ script.js
@@ -0,0 +1,125 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruits = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+const search = (str) => {
+  str = str.toLowerCase();
+
+  return fruits.filter((val) => val.toLowerCase().includes(str));
+};
+
+function searchHandler(e) {
+  const results = search(input.value);
+  //the results will populate based on what is in the input.value
+  showSuggestions(results, input.value);
+
+}
+
+function showSuggestions(results, inputVal) {
+  suggestions.innerText = "";
+  // the innerText of the results will be reset whenever the showSuggestions function is run
+
+  results.forEach(function (val) {
+    let li = document.createElement("li");
+    const newVal = val.replace(
+      inputVal.toLowerCase(),
+      `<strong>${inputVal.toLowerCase()}</strong>`
+    );
+    li.innerHTML = newVal;
+
+    suggestions.append(li);
+  });
+  if (inputVal === "") {
+    suggestions.innerText = "";
+  }
+}
+
+function useSuggestion(e) {
+  suggestions.innerText = "";
+  input.value = e.target.innerText;
+}
+
+input.addEventListener("keyup", searchHandler);
+
+suggestions.addEventListener("click", useSuggestion);
